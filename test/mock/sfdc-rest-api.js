cd6092bde149e247fb64d36ac9442bc8c8a2a1cd
Stephan H. Wissel <stw@linux.com>
2018-06-09 17:41:55 +1000

    Fixed version string

--- test/mock/sfdc-rest-api.js
+++ test/mock/sfdc-rest-api.js
@@ -1,4 +1,4 @@
-var http = require('http');
+var http = require("http");
 var port = process.env.PORT || 3000;
 var lastRequest = null;
 var nextResponse = null;
@@ -8,25 +8,22 @@ var isListening = false;
 var sockets = [];
 var server;
 
-const setPort = function (p) {
+const setPort = function(p) {
   port = p;
 };
 
-const start = function (port, cb) {
-
+const start = function(port, cb) {
   port = port || process.env.PORT || 3000;
 
-  server = http.createServer(function (req, res) {
-
+  server = http.createServer(function(req, res) {
     lastRequest = req;
-    lastRequest.body = '';
+    lastRequest.body = "";
 
-    req.on('data', function (chunk) {
+    req.on("data", function(chunk) {
       lastRequest.body += chunk.toString();
     });
 
-    req.on('end', function () {
-
+    req.on("end", function() {
       if (closeOnRequest) {
         if (sockets.length) {
           for (var i = 0; i < sockets.length; i++) {
@@ -39,35 +36,34 @@ const start = function (port, cb) {
       if (nextResponse) {
         res.writeHead(nextResponse.code, nextResponse.headers);
         if (nextResponse.body) {
-          res.write(nextResponse.body, 'utf8');
+          res.write(nextResponse.body, "utf8");
         }
       } else {
         res.writeHead(200, {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json"
         });
         res.write('{"Status":"OK"}');
       }
       res.end();
     });
-
   });
 
-  server.on('listening', function () {
+  server.on("listening", function() {
     isListening = true;
   });
 
-  server.on('close', function () {
+  server.on("close", function() {
     isListening = false;
   });
 
-  server.on('connection', function (socket) {
+  server.on("connection", function(socket) {
     sockets.push(socket);
-    socket.on('close', function () {
+    socket.on("close", function() {
       sockets.splice(sockets.indexOf(socket), 1);
     });
   });
 
-  server.listen(port, function (err) {
+  server.listen(port, function(err) {
     if (err) {
       return cb(err);
     }
@@ -76,32 +72,33 @@ const start = function (port, cb) {
 };
 
 // return an example client
-const getClient = function (opts) {
+const getClient = function(opts) {
   opts = opts || {};
   return {
-    clientId: 'ADFJSD234ADF765SFG55FD54S',
-    clientSecret: 'adsfkdsalfajdskfa',
-    redirectUri: 'http://localhost:' + port + '/oauth/_callback',
-    loginUri: 'http://localhost:' + port + '/login/uri',
-    apiVersion: opts.apiVersion || '43.0',
-    mode: opts.mode || 'multi',
+    clientId: "ADFJSD234ADF765SFG55FD54S",
+    clientSecret: "adsfkdsalfajdskfa",
+    redirectUri: "http://localhost:" + port + "/oauth/_callback",
+    loginUri: "http://localhost:" + port + "/login/uri",
+    apiVersion: opts.apiVersion || "v43.0",
+    mode: opts.mode || "multi",
     autoRefresh: opts.autoRefresh || false,
-    onRefresh: opts.onRefresh || undefined,
+    onRefresh: opts.onRefresh || undefined
   };
 };
 
 // return an example oauth
-const getOAuth = function () {
+const getOAuth = function() {
   return {
-    id: 'http://localhost:' + port + '/id/00Dd0000000fOlWEAU/005d00000014XTPAA2',
-    issued_at: '1362448234803',
-    instance_url: 'http://localhost:' + port,
-    signature: 'djaflkdjfdalkjfdalksjfalkfjlsdj',
-    access_token: 'aflkdsjfdlashfadhfladskfjlajfalskjfldsakjf'
+    id:
+      "http://localhost:" + port + "/id/00Dd0000000fOlWEAU/005d00000014XTPAA2",
+    issued_at: "1362448234803",
+    instance_url: "http://localhost:" + port,
+    signature: "djaflkdjfdalkjfdalksjfalkfjlsdj",
+    access_token: "aflkdsjfdlashfadhfladskfjlajfalskjfldsakjf"
   };
 };
 
-const setResponse = function (code, headers, body) {
+const setResponse = function(code, headers, body) {
   nextResponse = {
     code: code,
     headers: headers,
@@ -110,17 +107,17 @@ const setResponse = function (code, headers, body) {
 };
 
 // return the last cached request
-const getLastRequest = function () {
+const getLastRequest = function() {
   return lastRequest;
 };
 
 // simulate a socket close on a request
-const closeOnRequestFunc = function (close) {
+const closeOnRequestFunc = function(close) {
   closeOnRequest = close;
 };
 
 // reset the cache
-const reset = function () {
+const reset = function() {
   lastRequest = null;
   nextResponse = null;
   closeOnRequest = false;
@@ -128,7 +125,7 @@ const reset = function () {
 };
 
 // close the server
-const stop = function (cb) {
+const stop = function(cb) {
   if (!isListening) {
     server = null;
     return cb();
