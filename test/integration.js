c12ae2a4cda060f935522214d17bc6f18c5aa6ad
Stephan H. Wissel <stw@linux.com>
2018-06-10 13:14:30 +1000

    Skeleton for integration tests

---
+++ test/integration.js
@@ -0,0 +1,54 @@
+"use strict";
+/* End to End Tests - if environment parameters are found pointing to a Salesforce instance */
+const nforce = require("../");
+const should = require("should");
+
+// The SFDC Client instance
+let client = undefined;
+
+describe("Integration Test against an actual Salesforce instance", () => {
+  before(() => {
+    let creds = checkEnvCredentials();
+    if (creds == null) {
+      // Can't run integration tests
+      this.skip();
+    } else {
+      // TODO: fix the creds
+      client = nforce.createConnection(creds);
+    }
+  });
+
+  after(() => {
+    if (client != undefined) {
+      client.logout();
+    }
+  });
+
+  describe("Client session check", () => {
+    it("should have a valid client session", () => {
+      should.exists(client);
+    });
+  });
+});
+
+/* Checking if the environment has SFDC credentials, so we
+ * can run an integration test
+ */
+
+function checkEnvCredentials() {
+  let user = process.env.SFDC_USER;
+  let pwd = process.env.SFDC_PASSWORD;
+  let clientid = process.env.SFDC_CLIENTID;
+  let envType = process.env.SFDC_ENVIRONMENT;
+
+  if (user && pwd && clientid && envType) {
+    return {
+      user: user,
+      pwd: pwd,
+      clientid: clientid,
+      envType: envType
+    };
+  }
+
+  return null;
+}
